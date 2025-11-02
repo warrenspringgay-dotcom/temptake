1565135f0104d2b3b33e38c7686a8408160fa1b7
warren <warrenspringgay@gmail.com>
2025-11-02 22:07:38 +0000

    02.11.25

--- src/app/(protected)/allergens/page.tsx
+++ src/app/(protected)/allergens/page.tsx
@@ -1,12 +1,10 @@
 // src/app/(protected)/allergens/page.tsx
 "use client";
-import React from "react";
-import AllergenMatrix from "@/components/AllergenMatrix";
 
-export default function AllergenPage() {
-  return (
-    <div className="max-w-[1100px] mx-auto px-3 sm:px-4 py-4">
-      <AllergenMatrix />
-    </div>
-  );
+import AllergenManager from "@/components/AllergenManager";
+
+
+
+export default function AllergensPage() {
+  return <AllergenManager />;
 }
