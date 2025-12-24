5c7d2e98d2644dd23609d116deb5af9831f4cfba
warren <warrenspringgay@gmail.com>
2025-12-24 15:28:58 +0000

    24.12.25

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,20 +1,5 @@
-// src/app/(protected)/layout.tsx
-import React from "react";
-import WelcomeGate from "@/components/WelcomeGate";
-
-export const dynamic = "force-dynamic";
-
-export default async function ProtectedLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      {/* global chrome (nav, whatever) */}
-      {children}
-      {/* welcome modal lives here */}
-      <WelcomeGate />
-    </>
+    <div>test</div>
   );
 }
