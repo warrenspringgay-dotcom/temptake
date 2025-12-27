39e25c065a1e08a3f62876f07b81b88ff65e9886
warren <warrenspringgay@gmail.com>
2025-12-27 22:54:29 +0000

    27.12.25

--- src/app/(protected)/food-hygiene/page.tsx
+++ src/app/(protected)/food-hygiene/page.tsx
@@ -3,7 +3,7 @@ import FoodHygieneRatingLog from "@/components/FoodHygieneRatingLog";
 
 export default function FoodHygienePage() {
   return (
-    <main className="w-full px-3 sm:px-4 md:mx-auto md:max-w-6xl">
+    <main className="w-full -mx-3 px-3 sm:mx-0 sm:px-4 md:mx-auto md:max-w-6xl">
 
       <FoodHygieneRatingLog />
     </main>
