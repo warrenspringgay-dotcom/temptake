19664cf7a0ff03ac2a7707866b36308f9424f256
warrenspringgay-dotcom <warrenspringgay@gmail.com>
2026-03-12 15:40:29 +0000

    templates

--- public/robots.ts
+++ public/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/login", "/signup", "/dashboard"],
       },
     ],
-    sitemap: "https://temptake.app/sitemap.xml",
+    sitemap: "https://temptake.com/sitemap.xml",
   };
 }
